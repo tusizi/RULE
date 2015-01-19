bc8bd6f9adcf14e5c53523ea25ae90855caa8451
tusizi <529340225@qq.com>
2015-01-19 22:38:01 +0800

    login

--- src/main/java/cc/rule/controller/UserController.java
+++ src/main/java/cc/rule/controller/UserController.java
@@ -1,15 +1,28 @@
 package cc.rule.controller;
 
 import cc.rule.model.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by tusizi on 2015/1/18.
  */
-@RestController
+@Controller
 public class UserController {
+    @RequestMapping("/")
+    public String index(){
+        return "login";
+    }
+    @RequestMapping(value = "/user",method =RequestMethod.POST)
+    public String add(@RequestParam String username,@RequestParam String password){
+        if (username.equals("admin") && password.equals("admin"))
+            return "dashboard";
+        else
+        return "login";
+    }
     @RequestMapping(value="/user",method= RequestMethod.GET)
     public User user(){
         return  new User("xyn","12346");
