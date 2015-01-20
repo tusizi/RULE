d537ea69897d6da1dd209c147f36ce620d32cf5e
tusizi <529340225@qq.com>
2015-01-20 20:47:40 +0800

    denglu

--- src/main/java/cc/rule/controller/UserController.java
+++ src/main/java/cc/rule/controller/UserController.java
@@ -1,7 +1,9 @@
 package cc.rule.controller;
 
 import cc.rule.model.User;
+import org.omg.CORBA.Request;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,18 +15,36 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class UserController {
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         return "login";
     }
-    @RequestMapping(value = "/user",method =RequestMethod.POST)
-    public String add(@RequestParam String username,@RequestParam String password){
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String add(@RequestParam String username, @RequestParam String password) {
         if (username.equals("admin") && password.equals("admin"))
             return "dashboard";
         else
-        return "login";
+            return "login";
     }
-    @RequestMapping(value="/user",method= RequestMethod.GET)
-    public User user(){
-        return  new User("xyn","12346");
+
+    @RequestMapping(value = "/reg", method = RequestMethod.POST)
+    public String addReg(@RequestParam String username,
+                         @RequestParam String password,
+                         @RequestParam String rePassword,
+                         Model model) {
+        if (!password.equals(rePassword)) {
+            model.addAttribute("error", "密码不一致");
+            return "login";
+        } else if (username.equals("admin")) {
+            model.addAttribute("error", "用户已存在");
+            return "login";
+        }
+        return "dashboard";
+    }
+
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public User user() {
+        return new User("xyn", "12346");
     }
 }
--- src/main/java/cc/rule/model/User.java
+++ src/main/java/cc/rule/model/User.java
@@ -7,6 +7,9 @@ public class User {
     private  String name;
     private  String password;
 
+    public User(){
+
+    }
     public User(String name,String password){
         this.name = name;
         this.password = password;
